a3aa1df762b31878b49ed508c795afc7fa58f4a2
Caleb Severn <calsev@gmail.com>
2018-04-13 14:38:03 -0800

    Added helpers for time calcs

---
+++ libraries/motd_chef_status.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module MOTDChefStatus
+  # Some time calculations
+  module Helper
+    TCB = 'motd_chef_status'
+
+    def chef_client_interval_s
+      return Integer(Chef::Config[:interval]) if Chef::Config[:interval]
+      return Integer(node['chef_client']['interval']) if node.attribute?('chef_client')
+      return 1800 # The default is 30 minutes
+    end
+
+    def chef_client_splay_s
+      return Integer(Chef::Config[:splay]) if Chef::Config[:splay]
+      return Integer(node['chef_client']['splay']) if node.attribute?('chef_client')
+      return 300 # The default is 5 minutes
+    end
+
+    def chef_client_interval_minutes
+      return chef_client_interval_s / 60
+    end
+
+    def chef_client_splay_minutes
+      return chef_client_splay_s / 60
+    end
+
+    def chef_client_max_delay_minutes
+      return (chef_client_interval_s + chef_client_splay_s) / 60
+    end
+  end
+end
+
+Chef::Recipe.include(MOTDChefStatus::Helper)
+Chef::Resource.include(MOTDChefStatus::Helper)
