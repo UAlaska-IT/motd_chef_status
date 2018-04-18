f4b948026b96ad6a1149dc0a8ca686989321e51b
Caleb Severn <calsev@gmail.com>
2018-04-18 15:24:51 -0800

    Added a custom resource motd_fragment

---
+++ resources/motd_fragment.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+provides :motd_fragment
+
+property :fragment_name, String, name_property: true
+property :template_cookbook, String, required: true
+property :template_source, String, required: true
+property :template_variables, Hash, default: {}
+
+default_action :create
+
+action :create do
+  template fragment_directory + new_resource.fragment_name do
+    cookbook new_resource.template_cookbook
+    source new_resource.template_source
+    group node['root_group']
+    owner 'root'
+    mode '0755'
+    action :create
+  end
+end
+
+action :delete do
+  file fragment_directory + new_resource.fragment_name do
+    action :delete
+  end
+end
