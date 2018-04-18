31286d038eeba6305848e563c9be5405cd583cb1
Caleb Severn <calsev@gmail.com>
2018-04-18 15:44:55 -0800

    Added fragments for chef status templates

--- recipes/chef_status.rb
+++ recipes/chef_status.rb
@@ -2,6 +2,14 @@
 
 tcb = 'motd_chef_status'
 
+var_map = {
+    host_name: node['hostname'],
+    node_name_file: path_to_last_run_node_name,
+    success_file: path_to_last_run_success_flag,
+    maxium_delay: chef_client_max_delay_minutes,
+    timestamp_file: path_to_last_run_time
+}
+
 # Ubuntu desktop help
 motd_fragment '10-help-text' do
   action :delete
@@ -16,4 +24,16 @@ motd_fragment '51-cloudguest' do
   template_cookbook ''
   template_source ''
   template_variables {}
+end
+
+motd_fragment "#{node[tcb]['header_position']}-header" do
+  template_cookbook tcb
+  template_source 'motd-header.sh.erb'
+  template_variables var_map
+end
+
+motd_fragment "#{node[tcb]['header_position']}-chef-manage-status" do
+  template_cookbook tcb
+  template_source 'motd-chef-manage-status.sh.erb'
+  template_variables var_map
 end
