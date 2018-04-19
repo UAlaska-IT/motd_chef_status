72b3d9e322fe349a26378b28b45782e66ddb9a87
Caleb Severn <calsev@gmail.com>
2018-04-19 13:45:30 -0800

    Fixed package install on debian

--- recipes/install.rb
+++ recipes/install.rb
@@ -3,6 +3,5 @@
 include_recipe 'chef_run_recorder::default'
 
 # We use update-motd (/etc/update-motd.d) if available, otherwise profile.d
-package 'update-motd' do
-  only_if { node['platform_family'] == 'debian' }
-end
+
+package 'update-motd' if node['platform'] == 'ubuntu'
