c40edd12195e262e6d24aedba3ba2f259294fd1c
Caleb Severn <calsev@gmail.com>
2018-04-18 16:24:00 -0800

    Fixed use of node in test

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
-if node['platform_family'] == 'debian'
+if os[:family] == 'debian'
   fragment_dir = '/etc/update-motd.d/'
-elsif node['platform_family'] == 'redhat'
+elsif os[:family] == 'redhat'
   fragment_dir = '/etc/profile.d/'
 else
   raise "Platform family not supported: #{node['platform_family']}"
 end
 
 describe file(fragment_dir + '00-header') do
-  it {should exist}
+  it { should exist }
 end
 
 describe file(fragment_dir + '95-chef-manage-status') do
-  it {should exist}
+  it { should exist }
 end
