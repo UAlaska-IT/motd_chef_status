6e259f106bcd3420d736614750a43055ec933bce
Caleb Severn <calsev@gmail.com>
2018-08-25 19:13:01 -0800

    Added attributes for gating fragment removal

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,12 @@ tcb = 'motd_chef_status'
 
 default[tcb]['header_position'] = '00'
 default[tcb]['chef_status_position'] = '99'
+
+# Remove any existing fragments, if false attributes below have no effect
+default[tcb]['remove_existing_fragments'] = true
+
+# Documentation-related fragments
+default[tcb]['remove_document_fragments'] = true
+
+# Update-related fragments
+default[tcb]['remove_update_fragments'] = false
--- recipes/chef_status.rb
+++ recipes/chef_status.rb
@@ -2,6 +2,40 @@
 
 tcb = 'motd_chef_status'
 
+if node[tcb]['remove_existing_fragments']
+  # Still exists on Debian, will print on Ubuntu if it exists
+  file '/etc/motd' do
+    action :delete
+  end
+
+  # Debian host info
+  motd_fragment '10-uname' do
+    action :delete
+    template_cookbook ''
+    template_source ''
+  end
+
+  if node[tcb]['remove_document_fragments']
+    # Ubuntu desktop help
+    motd_fragment '10-help-text' do
+      action :delete
+      template_cookbook ''
+      template_source ''
+    end
+
+    # Ubuntu cloud help
+    motd_fragment '51-cloudguest' do
+      action :delete
+      template_cookbook ''
+      template_source ''
+    end
+  end
+
+  if node[tcb]['remove_update_fragments']
+
+  end
+end
+
 var_map = {
   host_name: node['hostname'],
   node_name_file: path_to_last_run_node_name,
@@ -9,32 +43,6 @@ var_map = {
   maxium_delay: chef_client_max_delay_minutes,
   timestamp_file: path_to_last_run_time
 }
-
-# Still exists on Debian, will print on Ubuntu if exist
-file '/etc/motd' do
-  action :delete
-end
-
-# Ubuntu desktop help
-motd_fragment '10-help-text' do
-  action :delete
-  template_cookbook ''
-  template_source ''
-end
-
-# Ubuntu cloud help
-motd_fragment '51-cloudguest' do
-  action :delete
-  template_cookbook ''
-  template_source ''
-end
-
-# Debian host info
-motd_fragment '10-uname' do
-  action :delete
-  template_cookbook ''
-  template_source ''
-end
 
 motd_fragment "#{node[tcb]['header_position']}-header#{fragment_extension}" do
   template_cookbook tcb
