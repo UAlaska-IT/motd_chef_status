62943cba941380cf2fc962898394fcdf805de2fb
Caleb Severn <calsev@gmail.com>
2018-08-27 09:50:18 -0800

    Split attributes file

---
+++ attributes/add_fragments.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+tcb = 'motd_chef_status'
+
+default[tcb]['add_header_fragment'] = true
+default[tcb]['add_chef_fragment'] = true
+
+default[tcb]['header_position'] = '00'
+default[tcb]['chef_status_position'] = '99'
--- attributes/remove_fragments.rb
+++ attributes/remove_fragments.rb
@@ -2,12 +2,6 @@
 
 tcb = 'motd_chef_status'
 
-default[tcb]['add_header_fragment'] = true
-default[tcb]['add_chef_fragment'] = true
-
-default[tcb]['header_position'] = '00'
-default[tcb]['chef_status_position'] = '99'
-
 # Remove any existing fragments, if false attributes below have no effect
 default[tcb]['remove_existing_fragments'] = true
 
